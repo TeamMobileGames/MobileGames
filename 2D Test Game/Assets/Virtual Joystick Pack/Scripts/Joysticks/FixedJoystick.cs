d0aff0435e015d4c5a1a4aa33bd9f310b68a387b
Q4drO <kimtischler1132@gmail.com>
2019-01-29 20:33:40 +0100

    Kim
    floating joystick added

--- 2D Test Game/Assets/Virtual Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
+++ 2D Test Game/Assets/Virtual Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
@@ -1,14 +1,57 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Runtime.InteropServices;
 
 public class FixedJoystick : Joystick
 {
+    public int joystickArea;
     Vector2 joystickPosition = Vector2.zero;
     private Camera cam = new Camera();
+    private Vector2 joystickAreaSize;
 
     void Start()
     {
+        if(joystickArea == 1)
+        {
+            joystickAreaSize = new Vector2(0,Screen.width/2);
+        }
+        else
+        {
+            joystickAreaSize = new Vector2(Screen.width / 2, Screen.width);
+        }
         joystickPosition = RectTransformUtility.WorldToScreenPoint(cam, background.position);
+    }
+
+    void Update()
+    {
+        if(Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            Debug.Log("touchCount  : " + Input.touchCount);
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (touch.position.x >= joystickAreaSize.x && touch.position.x <= joystickAreaSize.y)
+                {
+                    this.transform.position = new Vector3(touch.position.x, touch.position.y, 0);
+                    Input.GetMouseButtonDown(0);
+                }
+            }
+
+
+        }
+        else if (Input.touchCount == 2)
+        {
+            Touch touch = Input.GetTouch(1);
+            Debug.Log("touchCount  : " + Input.touchCount);
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (touch.position.x >= joystickAreaSize.x && touch.position.x <= joystickAreaSize.y)
+                {
+                    this.transform.position = new Vector3(touch.position.x, touch.position.y, 0);
+                }
+            }
+        }
+        
     }
 
     public override void OnDrag(PointerEventData eventData)
